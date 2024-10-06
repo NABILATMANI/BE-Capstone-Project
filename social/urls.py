e32cf0c39ace257bdc9447172fdfb739e78ea042
NABILATMANI <atmaninabile@gmail.com>
2024-10-06 16:29:23 +0100

    Update urls.py

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import PostViewSet, UserViewSet, FollowerViewSet
 from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, UserViewSet, FollowerViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
