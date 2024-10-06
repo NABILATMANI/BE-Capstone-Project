8837cadc4d8c32e67746da5be068a0d29bd7b79b
NABILATMANI <atmaninabile@gmail.com>
2024-10-06 14:14:52 +0100

    Update settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
