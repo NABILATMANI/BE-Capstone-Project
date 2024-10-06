84613fb321f276366a22c18139467e07b74adc86
NABILATMANI <atmaninabile@gmail.com>
2024-10-06 14:21:52 +0100

    Update wsgi.py

--- social_media_api/wsgi.py
+++ social_media_api/wsgi.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-# Make sure your project path is added to the system path
+# Add your project path to the system path
 path = '/home/nabilatmami/BE-Capstone-Project'
 if path not in sys.path:
     sys.path.append(path)
 
-# Set the DJANGO_SETTINGS_MODULE environment variable to point to your settings module
+# Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ['DJANGO_SETTINGS_MODULE'] = 'social_media_api.settings'
 
 # Load the WSGI application
