a8a4a98a5ae021fd0de5ec974c4d3eb70e0caac2
NABILATMANI <atmaninabile@gmail.com>
2024-10-06 14:19:06 +0100

    Update wsgi.py

--- social_media_api/wsgi.py
+++ social_media_api/wsgi.py
@@ -1,20 +1,14 @@
-"""
-WSGI config for social_media_api project.
-
-This script is used to serve your application in production using WSGI, 
-which is the Python standard for web servers and applications.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see:
-https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Make sure your project path is added to the system path
+path = '/home/nabilatmami/BE-Capstone-Project'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the DJANGO_SETTINGS_MODULE environment variable to point to your settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'social_media_api.settings'
+
+# Load the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-# Set the settings module for the 'social_media_api' project.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
-
-# Get the WSGI application for the project.
 application = get_wsgi_application()
