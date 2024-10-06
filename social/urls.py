5273bc93cab6d4fb2911b3f00637c7bf0e6b9ff2
NABILATMANI <atmaninabile@gmail.com>
2024-10-06 16:33:10 +0100

    Update urls.py

--- social/urls.py
+++ social/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, UserViewSet, FollowerViewSet
 
+# Définition du routeur pour la gestion des URLs REST
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'users', UserViewSet)
 router.register(r'followers', FollowerViewSet)
 
+# Les URLs générées par le routeur sont ajoutées au urlpatterns
 urlpatterns = router.urls
